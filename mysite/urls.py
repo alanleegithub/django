b341f81afcdf1bd5acfca719012aeca836c7b62d
alanleegithub <alanlee2005@gmail.com>
2014-07-09 17:25:35 +0800

    [Chapter 7] Forms - A Simple Form-Handling Example

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import patterns, include, url
 #from django.conf.urls.defaults import *
-from mysite.views import hello, current_datetime
+from mysite.views import hello, current_datetime, display_meta
 from django.contrib import admin
 admin.autodiscover()
+
+from books import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^hello/$', hello),
+    url(r'^meta/$', display_meta),
     url(r'^time/$', current_datetime),
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 )
--- mysite/views.py
+++ mysite/views.py
@@ -2,8 +2,17 @@ from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
 from django.shortcuts import render
+
 def hello(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello world %s %s %s" % (request.path, request.get_host(), request.get_full_path()))
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
 
 import datetime
 def current_datetime(request):
